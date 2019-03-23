f1785cefbf8823ff5f1f5be0c4d0ca3801bf7e7f
Zeeshan Khan <zkhan1093@gmail.com>
2019-03-24 00:03:01 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jmd_billing.wsgi
---
+++ jmd_billing/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jmd_billing project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jmd_billing.settings')
+
+application = get_wsgi_application()
